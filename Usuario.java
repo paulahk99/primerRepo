c0b9e27191153a041a6029abd83c904ff8904715
Paula Hernandez <paulahk@gmail.com>
2020-04-04 14:09:58 -0300

    primer commit clase Usuario

---
+++ Usuario.java
@@ -0,0 +1,15 @@
+
+public class Usuario{
+    private String nombre;
+    private String cedula;
+    private String contraseña;
+    private int edad;
+    private int cantidadHijos;
+    private int saldo = 0;
+    
+   
+    
+    
+}
+
+
