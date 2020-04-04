a454b82b01a2014743a58c38c59168539aaefba7
Paula Hernandez <paulahk@gmail.com>
2020-04-04 14:11:28 -0300

    segundo commit Usuario

--- Usuario.java
+++ Usuario.java
@@ -7,7 +7,62 @@ public class Usuario{
     private int cantidadHijos;
     private int saldo = 0;
     
-   
+   public Usuario(String unNombre, String unaCedula, String unaContraseña, int unaEdad, int unaCantidadHijos, int unSaldo){
+        this.setNombre(unNombre);
+        this.setCedula(unaCedula);
+        this.setContraseña(unaContraseña);
+        this.setEdad(unaEdad);
+        this.setCantidadHijos(unaCantidadHijos);
+        this.setSaldo(unSaldo);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCantidadHijos() {
+        return cantidadHijos;
+    }
+
+    public void setCantidadHijos(int cantidadHijos) {
+        this.cantidadHijos = cantidadHijos;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
     
     
 }
